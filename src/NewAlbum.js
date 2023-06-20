f805a2e0dbf9784056fe0d257e2c3fd879f9d729
prachigupta921 <gprachi921@gmail.com>
2023-06-20 12:43:32 +0530

    ch

--- src/NewAlbum.js
+++ src/NewAlbum.js
@@ -1,20 +1,19 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
+import {useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import "./CSS/NewAlbum.css";
 
 //This Is Adding New Album And Dummy API Call For POST
 
 function Newalbum(props) {
+  const navigate=useNavigate();
   const { albums, handleChangeAlbum } = props;
-
-  const [Id, setId] = useState();
-  const [userId, setUserID] = useState();
   const [title, setTitle] = useState("");
 
   function saveUser(e) {
     e.preventDefault();
-    let data = { Id, userId, title };
+    let data = { title };
     fetch("https://jsonplaceholder.typicode.com/albums", {
       method: "POST",
       body: JSON.stringify(data),
@@ -30,6 +29,7 @@ function Newalbum(props) {
         prevAlbums.push(newAlbum);
         handleChangeAlbum(prevAlbums);
         alert("ALbum added succefully")
+        navigate("/album")
     });
   }
 
@@ -51,28 +51,7 @@ function Newalbum(props) {
             }}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="Id">
-          <Form.Label>Id</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="ID"
-            value={Id}
-            onChange={(e) => {
-              setId(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="Title">
-          <Form.Label>UserID</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="UserID"
-            value={userId}
-            onChange={(e) => {
-              setUserID(e.target.value);
-            }}
-          />
-        </Form.Group>
+       
         <Button variant="primary" type="submit" onClick={saveUser}>
           Add To Album
         </Button>
